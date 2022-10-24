a6e0f37b94bee21ccedb3adfd843ab5ca0edcc80
VladimirVolvakov <dr.volvakov@gmail.com>
2022-10-24 22:37:49 +0300

    feat: pass props from Cards component to CardItem component

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,16 +1,15 @@
-// Images:
-import Card1 from "../assets/card1.jpg";
-
-const CardItem = () => {
+const CardItem = ({ image, title, subtitle }) => {
   return (
     <article className="relative rounded-xl">
       <div className="absolute h-full w-full bg-black/40 text-white rounded-xl">
-        <p className="text-2xl font-bold pt-4 px-2">Sun's Out, BOGO's Out</p>
-        <p className="px-2">Through 8/26</p>
-        <button className="absolute bottom-4 bg-white text-black border-white mx-2">Order Now</button>
+        <p className="text-2xl font-bold pt-4 px-2">{ title }</p>
+        <p className="px-2">{ subtitle }</p>
+        <button className="absolute bottom-4 bg-white text-black border-white mx-2">
+          Order Now
+        </button>
       </div>
       <img 
-        src={Card1} 
+        src={image} 
         className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
         alt="Headline Card" 
       />
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,12 +1,28 @@
 // Component:
 import CardItem from "./CardItem";
+// Images:
+import Card1 from "../assets/card1.jpg";
+import Card2 from "../assets/card2.jpg";
+import Card3 from "../assets/card3.jpg";
 
 const Cards = () => {
   return (
     <section className="max-w-[1440px] mx-auto px-4 py-12 grid md:grid-cols-3 gap-6">
-      <CardItem />
-      <CardItem />
-      <CardItem />
+      <CardItem 
+        image={Card1}
+        title="Sun's Out, BOGO's Out"
+        subtitle="Through 8/26"
+      />
+      <CardItem 
+        image={Card2} 
+        title="New Restaurants"
+        subtitle="Added Daily"
+      />
+      <CardItem 
+        image={Card3}
+        title="We Deliver Desserts"
+        subtitle="Tasty Treats" 
+      />
     </section>
   );
 };
