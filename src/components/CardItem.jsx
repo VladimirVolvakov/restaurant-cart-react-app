dd221f1a042c4a86359259e76a5a75eb5bac9b38
VladimirVolvakov <dr.volvakov@gmail.com>
2022-10-24 22:15:47 +0300

    feat: add markup & styles for CardItem component

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,8 +1,20 @@
+// Images:
+import Card1 from "../assets/card1.jpg";
+
 const CardItem = () => {
   return (
-    <div>
-      Card
-    </div>
+    <article className="relative rounded-xl">
+      <div className="absolute h-full w-full bg-black/40 text-white rounded-xl">
+        <p className="text-2xl font-bold pt-4 px-2">Sun's Out, BOGO's Out</p>
+        <p className="px-2">Through 8/26</p>
+        <button className="absolute bottom-4 bg-white text-black border-white mx-2">Order Now</button>
+      </div>
+      <img 
+        src={Card1} 
+        className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+        alt="Headline Card" 
+      />
+    </article>
   );
 };
 
