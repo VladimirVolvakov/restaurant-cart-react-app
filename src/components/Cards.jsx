23e87496137f0c1421d292d90f98f99831a446ef
VladimirVolvakov <dr.volvakov@gmail.com>
2022-10-24 22:44:42 +0300

    refactor: use map function to refactor code in Cards component

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,23 +6,37 @@ import Card2 from "../assets/card2.jpg";
 import Card3 from "../assets/card3.jpg";
 
 const Cards = () => {
+  const cardsInfo = [
+    {
+      image: Card1,
+      title: "Sun's Out, BOGO's Out",
+      subtitle: "Through 8/26"
+    },
+    {
+      image: Card2,
+      title: "New Restaurants",
+      subtitle: "Added Daily"
+    },
+    {
+      image: Card3,
+      title: "We Deliver Desserts",
+      subtitle: "Tasty Treats"
+    }
+  ];
+
   return (
     <section className="max-w-[1440px] mx-auto px-4 py-12 grid md:grid-cols-3 gap-6">
-      <CardItem 
-        image={Card1}
-        title="Sun's Out, BOGO's Out"
-        subtitle="Through 8/26"
+      {
+        cardsInfo.map((item, index) => (
+          <CardItem 
+            image={item.image}
+            title={item.title}
+            subtitle={item.subtitle}
+            key={index}
       />
-      <CardItem 
-        image={Card2} 
-        title="New Restaurants"
-        subtitle="Added Daily"
-      />
-      <CardItem 
-        image={Card3}
-        title="We Deliver Desserts"
-        subtitle="Tasty Treats" 
-      />
+        ))
+      }
+      
     </section>
   );
 };
