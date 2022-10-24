57a8451d5ae7104849fb24f6327de4119a65db01
VladimirVolvakov <dr.volvakov@gmail.com>
2022-10-24 22:23:55 +0300

    feat: add markup & styles to Cards component

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,7 +3,9 @@ import CardItem from "./CardItem";
 
 const Cards = () => {
   return (
-    <section>
+    <section className="max-w-[1440px] mx-auto px-4 py-12 grid md:grid-cols-3 gap-6">
+      <CardItem />
+      <CardItem />
       <CardItem />
     </section>
   );
