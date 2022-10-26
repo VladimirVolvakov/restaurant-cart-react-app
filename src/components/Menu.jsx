d14835cb2aa0ed8c31fb4410d23e80a9895929ab
VladimirVolvakov <dr.volvakov@gmail.com>
2022-10-27 00:00:50 +0300

    feat: implement filter by tood type functionality

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,9 +1,24 @@
+// Hook:
+import { useState } from "react";
 // Component:
 import MenuItem from "./MenuItem";
 // Data:
 import { categories, data } from "../data";
 
 const Menu = () => {
+  const [displayedFood, setDisplayedFood] = useState(data);
+
+  const burgers = data.filter(item => item.category === "burger");
+  const pizzas = data.filter(item => item.category === "pizza");
+  const salads = data.filter(item => item.category === "salad");
+  const chickens = data.filter(item => item.category === "chicken");
+
+  const displayAll = () => setDisplayedFood(data);
+  const displayedBurgers = () => setDisplayedFood(burgers);
+  const displayedPizzas = () => setDisplayedFood(pizzas);
+  const displayedSalads = () => setDisplayedFood(salads);
+  const displayedChickens = () => setDisplayedFood(chickens);
+
   return (
     <section className="max-w-[1440px] mx-auto px-4 py-12">
       <h2 className="text-4xl font-bold text-orange-500 text-center">Top Rated Menu Items</h2>
@@ -11,11 +26,11 @@ const Menu = () => {
         <div className="mt-4">
           <p className="font-bold text-gray-700 ml-1">Filter Type</p>
           <div className="flex justify-between flex-wrap">
-            <button className="border-orange-500 text-orange-500 hover:text-white hover:bg-orange-500 m-1">All</button>
-            <button className="border-orange-500 text-orange-500 hover:text-white hover:bg-orange-500 m-1">Burgers</button>
-            <button className="border-orange-500 text-orange-500 hover:text-white hover:bg-orange-500 m-1">Pizza</button>
-            <button className="border-orange-500 text-orange-500 hover:text-white hover:bg-orange-500 m-1">Salad</button>
-            <button className="border-orange-500 text-orange-500 hover:text-white hover:bg-orange-500 m-1">Chicken</button>
+            <button className="border-orange-500 text-orange-500 hover:text-white hover:bg-orange-500 m-1" onClick={displayAll}>All</button>
+            <button className="border-orange-500 text-orange-500 hover:text-white hover:bg-orange-500 m-1" onClick={displayedBurgers}>Burgers</button>
+            <button className="border-orange-500 text-orange-500 hover:text-white hover:bg-orange-500 m-1" onClick={displayedPizzas}>Pizza</button>
+            <button className="border-orange-500 text-orange-500 hover:text-white hover:bg-orange-500 m-1" onClick={displayedSalads}>Salad</button>
+            <button className="border-orange-500 text-orange-500 hover:text-white hover:bg-orange-500 m-1" onClick={displayedChickens}>Chicken</button>
           </div>
         </div>
         <div className="mt-4">
@@ -30,7 +45,7 @@ const Menu = () => {
       </div>
 
       <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4">
-        { data.map(item => (
+        { displayedFood.map(item => (
           <MenuItem item={item} />
         )) }
       </div>
