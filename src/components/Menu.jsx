01964db9394a406539ec1db68f97a02a1558a261
VladimirVolvakov <dr.volvakov@gmail.com>
2022-10-29 23:49:58 +0300

    refactor: change code for filter menu items by type functionality

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,21 +3,18 @@ import { useState } from "react";
 // Component:
 import MenuItem from "./MenuItem";
 // Data:
-import { categories, data } from "../data";
+import { data } from "../data";
 
 const Menu = () => {
   const [displayedFood, setDisplayedFood] = useState(data);
 
-  const burgers = data.filter(item => item.category === "burger");
-  const pizzas = data.filter(item => item.category === "pizza");
-  const salads = data.filter(item => item.category === "salad");
-  const chickens = data.filter(item => item.category === "chicken");
-
   const displayAll = () => setDisplayedFood(data);
-  const displayedBurgers = () => setDisplayedFood(burgers);
-  const displayedPizzas = () => setDisplayedFood(pizzas);
-  const displayedSalads = () => setDisplayedFood(salads);
-  const displayedChickens = () => setDisplayedFood(chickens);
+
+  const filterItemsByType = type => {
+    setDisplayedFood(data.filter(item => {
+      return item.category === type;
+    }));
+  };
 
   const filterItemsByPrice = price => {
     setDisplayedFood(data.filter(item => {
@@ -30,17 +27,17 @@ const Menu = () => {
       <h2 className="text-4xl font-bold text-orange-500 text-center">Top Rated Menu Items</h2>
       <div className="flex flex-col lg:flex-row justify-between">
         <div className="mt-4">
-          <p className="font-bold text-gray-700 ml-1">Filter Type</p>
+          <p className="font-bold text-gray-700 ml-1">Filter By Type</p>
           <div className="flex justify-between flex-wrap">
             <button className="border-orange-500 text-orange-500 hover:text-white hover:bg-orange-500 m-1" onClick={displayAll}>All</button>
-            <button className="border-orange-500 text-orange-500 hover:text-white hover:bg-orange-500 m-1" onClick={displayedBurgers}>Burgers</button>
-            <button className="border-orange-500 text-orange-500 hover:text-white hover:bg-orange-500 m-1" onClick={displayedPizzas}>Pizza</button>
-            <button className="border-orange-500 text-orange-500 hover:text-white hover:bg-orange-500 m-1" onClick={displayedSalads}>Salad</button>
-            <button className="border-orange-500 text-orange-500 hover:text-white hover:bg-orange-500 m-1" onClick={displayedChickens}>Chicken</button>
+            <button className="border-orange-500 text-orange-500 hover:text-white hover:bg-orange-500 m-1" onClick={() => filterItemsByType('burger')}>Burgers</button>
+            <button className="border-orange-500 text-orange-500 hover:text-white hover:bg-orange-500 m-1" onClick={() => filterItemsByType('pizza')}>Pizza</button>
+            <button className="border-orange-500 text-orange-500 hover:text-white hover:bg-orange-500 m-1" onClick={() => filterItemsByType('salad')}>Salad</button>
+            <button className="border-orange-500 text-orange-500 hover:text-white hover:bg-orange-500 m-1" onClick={() => filterItemsByType('chicken')}>Chicken</button>
           </div>
         </div>
         <div className="mt-4">
-          <p className="font-bold text-gray-700 ml-1">Filter Price</p>
+          <p className="font-bold text-gray-700 ml-1">Filter By Price</p>
           <div className="w-full max-w-[390px] flex">
             <button className="border-orange-500 text-orange-500 hover:text-white hover:bg-orange-500 m-1" onClick={() => filterItemsByPrice('$')}>$</button>
             <button className="border-orange-500 text-orange-500 hover:text-white hover:bg-orange-500 m-1" onClick={() => filterItemsByPrice('$$')}>$$</button>
