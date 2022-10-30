4196f99091c51c8b1732fa6ef83e77cbbe6bba72
VladimirVolvakov <dr.volvakov@gmail.com>
2022-10-30 21:31:53 +0300

    style: add styles for Category component

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -5,10 +5,12 @@ import { categories } from "../data";
 
 const Category = () => {
   return (
-    <section>
-      <h2>Our Food Categories</h2>
-      <div>
-        { categories.map(category => (<CategoryItem key={category.id} category={category} />)) }
+    <section className="max-w-[1440px] px-4 py-12 mx-auto">
+      <h2 className="text-orange-500 text-4xl font-bold text-center">Our Food Categories</h2>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-6 py-6">
+        { categories.map(category => (
+          <CategoryItem key={category.id} category={category} />
+        )) }
       </div>
     </section>
   );
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -26,7 +26,7 @@ const Menu = () => {
     <section className="max-w-[1440px] mx-auto px-4 py-12">
       <h2 className="text-4xl font-bold text-orange-500 text-center">Top Rated Menu Items</h2>
       <div className="flex flex-col lg:flex-row justify-between">
-        <div className="mt-4">
+        <div className="mt-6">
           <p className="font-bold text-gray-700 ml-1">Filter By Type</p>
           <div className="flex justify-between flex-wrap">
             <button className="border-orange-500 text-orange-500 hover:text-white hover:bg-orange-500 m-1" onClick={displayAll}>All</button>
@@ -36,7 +36,7 @@ const Menu = () => {
             <button className="border-orange-500 text-orange-500 hover:text-white hover:bg-orange-500 m-1" onClick={() => filterItemsByType('chicken')}>Chicken</button>
           </div>
         </div>
-        <div className="mt-4">
+        <div className="mt-6">
           <p className="font-bold text-gray-700 ml-1">Filter By Price</p>
           <div className="w-full max-w-[390px] flex">
             <button className="border-orange-500 text-orange-500 hover:text-white hover:bg-orange-500 m-1" onClick={() => filterItemsByPrice('$')}>$</button>
