eb8a1a1c89328b0185dd22588527b5c6d7711c81
VladimirVolvakov <dr.volvakov@gmail.com>
2022-10-30 21:55:25 +0300

    style: add styles for CategoryItem component

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,8 +1,8 @@
 const CategoryItem = ({ category }) => {
   return (
-    <article>
-      <h2>{ category.name }</h2>
-      <img src={ category.image } alt={ category.name } />
+    <article className="bg-gray-100 p-4 rounded-lg shadow-lg cursor-pointer hover:scale-105 duration-300 flex justify-between items-center">
+      <h2 className="sm:text-xl font-bold">{ category.name }</h2>
+      <img className="w-20" src={ category.image } alt={ category.name } />
     </article>
   );
 };
